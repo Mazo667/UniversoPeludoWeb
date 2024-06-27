58719d23d7f1e8fe9bbf2b338f2fae6971373dbb
Mazo667 <maxifava667@gmail.com>
2024-06-27 02:24:22 -0300

    Inicio con el acordion en intranet

--- scripting/desplegables.js
+++ scripting/desplegables.js
@@ -1,20 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var accordions = document.querySelectorAll('.accordion');
-
-  accordions.forEach(function (accordion) {
-    accordion.addEventListener('click', function () {
-      // Alternar la clase 'active' en el acordeón
-      this.classList.toggle('active');
-
-      // Seleccionar el panel siguiente al acordeón
-      var panel = this.nextElementSibling;
-
-      // Alternar la expansión y colapso del panel
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  });
+$('.head').click(function(){
+  $(this).toggleClass('active');
+  $(this).parent().find('.arrow').toggleClass('arrow-animate');
+  $(this).parent().find('.content').slideToggle(280);
 });
+
+// icon from Font Awesome was used in accordion-1
